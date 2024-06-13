2019167f0f63f14985ddf65e67198d334e7d76a1
f-mohrechi <f.mohrechi78@gmail.com>
2024-06-13 15:19:45 +0330

    modified:   src/app/page.js 	modified:   src/components/coinBox/CoinBox.jsx 	deleted:    src/components/exchhange/List.jsx 	deleted:    src/components/exchhange/ListHeader.jsx 	deleted:    src/components/exchhange/ListItem.jsx 	modified:   src/components/form/Form.jsx 	modified:   tailwind.config.js

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -4,7 +4,7 @@ import PrimaryButton from "../buttons/PrimaryButton";
 
 export default function Form() {
   return (
-    <div className="bg-primary-300 border border-primary-300 rounded-3xl p-5 flex flex-col shadow-main">
+    <div className="bg-primary-300 border border-primary-300 rounded-3xl p-5 flex flex-col">
       <div className="mb-5">
         <p className="text-2xl font-semibold">Buy</p>
       </div>
