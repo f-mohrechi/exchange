2a919e9b0b6ad029dd91f31d9269b7269ad3cec9
f-mohrechi <f.mohrechi78@gmail.com>
2024-06-13 18:07:59 +0330

    add box shadow to list

--- src/components/exchhange/List.jsx
+++ src/components/exchhange/List.jsx
@@ -5,7 +5,7 @@ import ListHeader from "./ListHeader";
 export default function List({ data }) {
   return (
     <div className="overflow-x-auto">
-      <table className="min-w-full bg-primary-300 rounded-xl">
+      <table className="min-w-full bg-primary-300 rounded-xl shadow-main">
         <ListHeader />
         <tbody>
           {data.map((item, index) => (
